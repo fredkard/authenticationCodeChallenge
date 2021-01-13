2bbbdf284ae95c408fcde427201dab69283a75b8
fredy kardian <fred.kard@gmail.com>
2021-01-13 14:27:15 +0700

    add route - file upload

--- src/http/route/route.js
+++ src/http/route/route.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const authController = require('../../logic/auth/auth.controller');
 const userController = require('../../logic/user/user.controller');
+const fileController = require('../../logic/file/file.controller');
 const { JWTMiddleware } = require('../middleware/JWT.middleware');
 
 const router = express.Router();
 
 const ac = new authController();
 const uc = new userController();
+const fc = new fileController();
 
 router.post('/auth', ac.auth);
 router.get('/user/profile', JWTMiddleware, uc.profile);
+router.post('/file/upload', JWTMiddleware, fc.upload);
 
 module.exports = router
