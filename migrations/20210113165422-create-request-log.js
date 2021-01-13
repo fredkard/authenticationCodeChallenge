8ff24dea43fa56d265ffe5d8b95b3ca24cb095c7
fredy kardian <fred.kard@gmail.com>
2021-01-14 00:45:52 +0700

    add migration

---
+++ migrations/20210113165422-create-request-log.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('requestLogs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userid: {
+        type: Sequelize.STRING
+      },
+      result_status: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('requestLogs');
+  }
+};
