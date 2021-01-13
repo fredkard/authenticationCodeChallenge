8680c8a6b02dbc33c114fece3edcc51e280ec46f
ariefdfaltah <arief.dfaltah@gmail.com>
2021-01-13 10:10:12 +0700

    update index.js

--- index.js
+++ index.js
@@ -5,15 +5,18 @@ const routes = require('./src/http/route/route');
 const CORSMiddleware = require('./src/http/middleware/CORS.middleware');
 const responseMiddleware = require('./src/http/middleware/response.middleware');
 
+const env = require('dotenv');
+env.config();
+
 app.use(bodyParser.json())
 app.use(CORSMiddleware);
 
 app.set('json spaces', 2)
 
-app.use('/', routes);
+app.use('/api', routes);
 
 app.use(responseMiddleware);
 
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
-});
+app.listen(process.env.PORT, () => {
+    console.log(`Server is listening on port ${process.env.PORT} with Application Name: ${process.env.APPLICATION_NAME}`);
+  });
